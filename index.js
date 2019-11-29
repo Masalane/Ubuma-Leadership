4bf604e19ab31eebab188abdcc5f2d2714213d9d
Masalane <1408187@students.wits.ac.za>
2019-11-29 17:06:59 +0200

    Welcome Page

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ let app = express()
 
 app.use(require('./dashBoard'))
 
+// serving static files
+app.use('/cdn', express.static('public'))
+
 app.listen(3000)
 console.log('Express server running on port 3000')
