fde4c4202a757d34d45dc9cd94c8ade255975e68
Gerad <gerrym751@gmail.com>
2019-11-29 20:34:48 +0200

    Editing with live server

--- dashBoard.js
+++ dashBoard.js
@@ -4,8 +4,8 @@ let path = require('path')
 let express = require('express')
 let dashBoard = express.Router()
 
-dashBoard.get('/about', function (req, res) {
-  res.sendFile(path.join(__dirname, 'views', 'dashBoard.html'))
+dashBoard.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'views','dashBoard.html'))
 })
 
 module.exports = dashBoard
